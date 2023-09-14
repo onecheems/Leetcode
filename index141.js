c98a5eebee42c39fd63a041b9f0d3be404d3871f
onecheems <1514873543@qq.com>
2023-09-14 10:54:05 +0800

    feat:141

---
+++ index141.js
@@ -0,0 +1,14 @@
+// @param {ListNode} head
+// @return {boolean}
+
+var hasCycle = function (head) {
+    let slow = head, fast = head
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+        if (fast === slow) {
+            return true
+        }
+    }
+    return false
+};
