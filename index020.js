f02bfcd69afb0a18928aa151a298ad5e4184823e
onecheems <1514873543@qq.com>
2023-09-16 15:15:03 +0800

    feat:020

---
+++ index020.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const length = s.length
+    if (length % 2 == 1) {
+        return false
+    }
+    const pairs = new Map([
+        [')', '('],
+        [']', '['],
+        ['}', '{']
+    ])
+    const stk = []
+    for (let ch of s){
+        if(pairs.has(ch)){
+            if(!stk.length||stk[stk.length-1]!==pairs.get(ch)){
+                return false
+            }
+            stk.pop()
+        }
+        else{
+            stk.push(ch)
+        }
+    }
+    return !stk.length
+};
